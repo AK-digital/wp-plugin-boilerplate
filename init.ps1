65e006aa373a0bf2e9fc0bf145bddf1604320b8f
Aurélien Kohler <aurelien@akdigital.fr>
2023-10-06 00:21:55 +0200

    update init.ps1 and init.sh

--- init.ps1
+++ init.ps1
@@ -1,54 +1,70 @@
-# Inviter l'utilisateur à entrer la nouvelle valeur pour $PLUGIN_NAME_HYPHEN
-$PLUGIN_NAME_HYPHEN = Read-Host -Prompt "Entrez le nom du plugin (en minuscules et avec des tirets)"
+# Invite the user to enter the new value for $PLUGIN_NAME_HYPHEN
+do {
+    $PLUGIN_NAME_HYPHEN = Read-Host -Prompt "Enter the plugin name (in lowercase, without spaces and with hyphens)"
+    if ($PLUGIN_NAME_HYPHEN -cmatch '[A-Z]' -or $PLUGIN_NAME_HYPHEN -match '\s') {
+        $PLUGIN_NAME_HYPHEN = $null  # Réinitialiser la valeur pour que la boucle continue
+        Write-Host "Error : Unauthorized characters or spaces."
+    }
+} while ([string]::IsNullOrWhiteSpace($PLUGIN_NAME_HYPHEN) -or $PLUGIN_NAME_HYPHEN -notmatch '^[a-z0-9-]+$')
 
-# Déduire $PLUGIN_NAME_UNDERSCORE à partir de $PLUGIN_NAME_HYPHEN
+
+# Derive $PLUGIN_NAME_UNDERSCORE from $PLUGIN_NAME_HYPHEN
 $PLUGIN_NAME_UNDERSCORE = $PLUGIN_NAME_HYPHEN -replace "-", "_"
 
 $PLUGIN_NAME_CLASSCASE = (Get-Culture).TextInfo.ToTitleCase($PLUGIN_NAME_UNDERSCORE.ToLower())
 
-# Déduire $PLUGIN_NAME_HYPHEN_UPPERCASE à partir de $PLUGIN_NAME_HYPHEN
+# Derive $PLUGIN_NAME_CLASSCASE from $PLUGIN_NAME_UNDERSCORE
 $VersionConst = $PLUGIN_NAME_UNDERSCORE.ToUpper() + "_VERSION"
 
-# Pré-remplir le chemin du plugin
+# Pre-fill the plugin path
 $PLUGIN_DIR = "./plugin-name"
 
-# Chemin du fichier plugin-name.php
+# Path of the file plugin-name.php
 $PLUGIN_FILE = "$PLUGIN_DIR/plugin-name.php"
 
-# # Inviter l'utilisateur à entrer les nouvelles données pour le fichier plugin-name.php
-$PluginName = Read-Host -Prompt "Plugin Name"
+# Invite the user to enter the new data for the file plugin-name.php
+do {
+    $PluginName = Read-Host -Prompt "Plugin Name (required)"
+} while ([string]::IsNullOrWhiteSpace($PluginName))
 $PluginURI = Read-Host -Prompt "Plugin URI"
 $Description = Read-Host -Prompt "Description"
 $Author = Read-Host -Prompt "Author"
 $AuthorURI = Read-Host -Prompt "Author URI"
-$TextDomain = Read-Host -Prompt "Text Doman"
-
-# # Obtenir l'URL du dépôt Git distant
-$gitRemoteUrl = git -C $PLUGIN_DIR remote get-url origin
-
-# # Vérifier si l'URL du dépôt Git distant a été obtenue
-if (-not $gitRemoteUrl) {
-    Write-Host "Erreur : Impossible d'obtenir l'URL du dépôt Git distant."
-    exit 1
+$License = Read-Host -Prompt "License (default is GPL-2.0-or-later)"
+if ([string]::IsNullOrWhiteSpace($License)) {
+    $License = "GPL-2.0-or-later"
+}
+$LicenseURI = Read-Host -Prompt "License URI (default is https://www.gnu.org/licenses/gpl-2.0.html)"
+if ([string]::IsNullOrWhiteSpace($License)) {
+    $License = "https://www.gnu.org/licenses/gpl-2.0.html"
 }
 
-# # Lire le contenu du fichier plugin-name.php
+# Obtain the URL of the remote Git repository
+$gitRemoteUrl = git -C $PLUGIN_DIR remote get-url origin
+
+# Check if the URL of the remote Git repository has been obtained
+if (-not $gitRemoteUrl) {
+    $gitRemoteUrl = ""
+}
+
+# Read the content of the file plugin-name.php
 $Content = Get-Content -Path $PLUGIN_FILE
 
-# # Remplacer les anciennes données par les nouvelles données
+# Replace the old data with the new data
 $Content = $Content -replace "WordPress Plugin Boilerplate", $PluginName `
     -replace "http://example.com/plugin-name-uri/", $PluginURI `
     -replace "This is a short description of what the plugin does. It's displayed in the WordPress admin area.", $Description `
     -replace "Your Name or Your Company", $Author `
     -replace "http://example.com/", $AuthorURI `
-    -replace "plugin-name", $TextDomain `
     -replace '(?<=@link\s).*', $gitRemoteUrl `
+    -replace "GPL-2.0+", $License `
+    -replace "http://www.gnu.org/licenses/gpl-2.0.txt", $LicenseURI `
     -replace "PLUGIN_NAME_VERSION", $VersionConst
 
-# # Écrire le nouveau contenu dans le fichier plugin-name.php
+# Write the new content in the file plugin-name.php
 $Content | Out-File -FilePath $PLUGIN_FILE
 
-# Fonction pour renommer les fichiers
+# Function to rename files
 Function Rename-Files {
     Param (
         [string]$directory
@@ -70,23 +86,31 @@ Function Replace-Text {
             # Read the entire file content at once
             $content = Get-Content $filePath -Raw -Encoding UTF8
 
-            # Replace text using the [regex]::Replace method for case-sensitive replacement
-            $content = [regex]::Replace($content, 'plugin_name', $PLUGIN_NAME_UNDERSCORE)
-            $content = [regex]::Replace($content, 'plugin-name', $PLUGIN_NAME_HYPHEN)
-            $content = [regex]::Replace($content, 'Plugin_Name', $PLUGIN_NAME_CLASSCASE)
+            # Check if content is null or empty before proceeding
+            if ([string]::IsNullOrWhiteSpace($content)) {
+                Write-Host "Warning: File $filePath is empty or inaccessible."
+            } else {
+                # Replace text using the [regex]::Replace method for case-sensitive replacement
+                $content = [regex]::Replace($content, 'plugin_name', $PLUGIN_NAME_UNDERSCORE)
+                $content = [regex]::Replace($content, 'plugin-name', $PLUGIN_NAME_HYPHEN)
+                $content = [regex]::Replace($content, 'Plugin_Name', $PLUGIN_NAME_CLASSCASE)
 
-            # Write the modified content back to the file
-            $content | Set-Content $filePath -Encoding UTF8
+                # Write the modified content back to the file
+                $content | Set-Content $filePath -Encoding UTF8
+            }
         } catch {
             Write-Host "Error: $_"
         }
     }
 }
 
-# Appeler les fonctions
+# Call the functions
 Rename-Files -directory $PLUGIN_DIR
 Replace-Text -directory $PLUGIN_DIR
 
-# Renommer le dossier du plugin
+# Rename the plugin folder
 $NewPluginDir = $PLUGIN_DIR -replace "plugin-name", $PLUGIN_NAME_HYPHEN
 Rename-Item -Path $PLUGIN_DIR -NewName $NewPluginDir
+
+# Cheers!
+Write-Host "Done. Your new plugin project is all set !" -ForegroundColor Green
--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Inviter l'utilisateur à entrer la nouvelle valeur pour PLUGIN_NAME_HYPHEN
-read -p "Entrez le nom du plugin (en minuscules et avec des tirets) : " PLUGIN_NAME_HYPHEN
+read -p "Entrez le nom du plugin (en minuscules et avec des tirets): " PLUGIN_NAME_HYPHEN
 
 # Déduire PLUGIN_NAME_UNDERSCORE à partir de PLUGIN_NAME_HYPHEN
 PLUGIN_NAME_UNDERSCORE=${PLUGIN_NAME_HYPHEN//-/_}
 
 # Convertir en CamelCase pour les noms de classe
-PLUGIN_NAME_CLASSCASE=$(echo "$PLUGIN_NAME_UNDERSCORE" | awk -F"_" '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1' OFS="_")
+PLUGIN_NAME_CLASSCASE=$(echo "$PLUGIN_NAME_UNDERSCORE" | awk 'BEGIN{FS=OFS="_"} {for(i=1;i<=NF;i++)$i=toupper(substr($i,1,1)) tolower(substr($i,2))} 1')
 
 # Déduire PLUGIN_NAME_HYPHEN_UPPERCASE à partir de PLUGIN_NAME_HYPHEN
 VersionConst="${PLUGIN_NAME_UNDERSCORE^^}_VERSION"
@@ -19,12 +19,11 @@ PLUGIN_DIR="./plugin-name"
 PLUGIN_FILE="$PLUGIN_DIR/plugin-name.php"
 
 # Inviter l'utilisateur à entrer les nouvelles données pour le fichier plugin-name.php
-read -p "Plugin Name : " PluginName
-read -p "Plugin URI : " PluginURI
-read -p "Description : " Description
-read -p "Author : " Author
-read -p "Author URI : " AuthorURI
-read -p "Text Domain : " TextDomain
+read -p "Plugin Name: " PluginName
+read -p "Plugin URI: " PluginURI
+read -p "Description: " Description
+read -p "Author: " Author
+read -p "Author URI: " AuthorURI
 
 # Obtenir l'URL du dépôt Git distant
 gitRemoteUrl=$(git -C "$PLUGIN_DIR" remote get-url origin)
@@ -41,11 +40,10 @@ Content=$(cat "$PLUGIN_FILE")
 # Remplacer les anciennes données par les nouvelles données
 Content=$(echo "$Content" | \
     sed -e "s|WordPress Plugin Boilerplate|$PluginName|g" \
-        -e "s|http://example.com/plugin-name-url/|$PluginURI|g" \
+        -e "s|http://example.com/plugin-name-uri/|$PluginURI|g" \
         -e "s|This is a short description of what the plugin does. It's displayed in the WordPress admin area.|$Description|g" \
         -e "s|Your Name or Your Company|$Author|g" \
         -e "s|http://example.com/|$AuthorURI|g" \
-        -e "s|plugin-name|$TextDomain|g" \
         -e "s|(?<=@link\s).*|$gitRemoteUrl|g" \
         -e "s|PLUGIN_NAME_VERSION|$VersionConst|g")
 
@@ -55,15 +53,15 @@ echo "$Content" > "$PLUGIN_FILE"
 # Fonction pour renommer les fichiers
 rename_files() {
     local directory=$1
-    find "$directory" -type f -name "*plugin-name*" | while read file; do
+    find "$directory" -type f -name "*plugin-name*" | while read -r file; do
         mv "$file" "${file//plugin-name/$PLUGIN_NAME_HYPHEN}"
     done
 }
 
-# Function to replace text in files
+# Fonction pour remplacer le texte dans les fichiers
 replace_text() {
     local directory=$1
-    find "$directory" -type f | while read file; do
+    find "$directory" -type f | while read -r file; do
         sed -i \
             -e "s|plugin_name|$PLUGIN_NAME_UNDERSCORE|g" \
             -e "s|plugin-name|$PLUGIN_NAME_HYPHEN|g" \
